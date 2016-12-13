1106bb96134b14800a0c20ce482271a59aed6831
annerufa <rufaedah@gmail.com>
2016-12-13 12:08:14 +0700

    Update m_laporan.php

--- app/m_laporan.php
+++ app/m_laporan.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class m_laporan extends Model
+class m_anggota extends Model
 {
-       protected $primaryKey = 'id_laporan';
-       public function petani(){
-		return $this->belongsTo(m_anggota::class,'id_petani');
+    protected $primaryKey = 'id_anggota';
+    protected $table = 'tb_anggota';
+    public function scopeAnggota($query) {
+		return $query->where('status', 'petani');
 	}
+	public $timestamps = false;
 }
