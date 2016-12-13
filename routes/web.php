7f8cbae6cd564ce6dfdd0a7f1f9a2442c6dbcc09
annerufa <rufaedah@gmail.com>
2016-12-13 12:14:15 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,34 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
 
-route::get('/', function(){
-	return redirect()->to('/petani');
+
+Route::post('cekKondisi','c_cek@kondisi');
+
+Route::get('/', function () {
+    return view('login');
 });
-Route::resource('petani', 'c_petani');
-Route::resource('ketua', 'c_ketua');
-Route::resource('anggota', 'c_anggota');
-Route::resource('cek', 'c_cek');
-Route::resource('laporan','c_laporan');
+Route::get('/ph', function () {
+    return view('v_cekph');
+});
+//Login
+Route::post('semaput','c_login@login');
+Route::get('logout','c_login@logout');
+//Ketua
+Route::get('Ketua','c_ketuaPetani@index');
+Route::get('laporanAnalisis','c_ketuaPetani@showLaporanAnalisis');
+Route::get('dataAnggota','c_ketuaPetani@showDataAnggota');
+Route::post('Anggota','c_ketuaPetani@tambahAnggota');
+Route::delete('hapusAnggota/{id_petani}','c_ketuaPetani@hapusAnggota');
+//Anggota
+Route::post('profil','c_petani@ubahProfil');
+Route::get('Petani','c_petani@index');
+//analisis
+Route::get('Analisis','c_petani@ShowFormDataLahan');
+Route::post('kondisiTanah','c_petani@simpanUmurLuas');
+Route::post('warnaDaun','c_petani@simpanWarnaDaun');
+Route::post('unsurP','c_petani@simpanUnsurP');
+Route::post('unsurK','c_petani@simpanUnsurK');
+Route::post('ph','c_petani@tampilHasil');
+Route::get('tersimpan','c_petani@simpanHasil');
+Route::get('home','c_petani@batalSimpan');
 
-Route::get('cekn','c_cek@cekn');
-Route::get('cekp','c_cek@cekp');
-Route::get('cekk','c_cek@cekk');
-Route::get('cekph','c_cek@cekph');
